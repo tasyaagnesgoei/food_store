0e11d0cf027cfe488761105fa0498351f4a1121a
BuildWith Angga <buildwithangga@BuildWiths-MacBook-Pro.local>
2021-06-19 23:35:57 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle foodTitle = GoogleFonts.poppins(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle pricingTextStyle = GoogleFonts.poppins(
+  color: Color(0xffB0B0B0),
+  fontSize: 14,
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: Color(0xff000000),
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
